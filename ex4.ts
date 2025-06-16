3210f6ac683c69aca9d9202d046e3de56098c07f
tomerfFeedU <tomerf@feed-u.co>
2025-06-16 18:19:45 +0300

    task 1 and 2

--- ex4.ts
+++ ex4.ts
@@ -1,21 +1,57 @@
-//Q1
-export function all<T>(promises : Array<Promise<T>>) : Promise<Array<T>> {
+import { values } from "ramda";
 
-  return new Promise<T[]>( (resolve, reject) => {
-    resolve([]);
-    //TODO
+//Q1
+export function all<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
+  return new Promise<T[]>((resolve, reject) => {
+    const res: T[] = [];
+    let resolvedCounter = 0;
+
+    if (promises.length === 0) {
+      // No promises
+      resolve([]);
+    }
+
+    promises.forEach((promise, idx) => {
+      promise
+        .then((value) => {
+          // When promise is finish, take value and do the following
+          res[idx] = value;
+          resolvedCounter++;
+          if (resolvedCounter === promises.length) {
+            resolve(res); // Finish successfully, returning the result
+          }
+        })
+        .catch((err) => {
+          reject(err); // Something failed
+        });
+    });
   });
 }
 
-  
 // Q2
 export function* Fib1() {
-	// @TODO
-  return undefined;
+  let fst = 1;
+  let sec = 1;
+  yield fst;
+  yield sec;
+
+  while (true) {
+    let next = fst + sec;
+    fst = sec;
+    sec = next;
+    yield next;
+  }
 }
 
-
 export function* Fib2() {
-	// @TODO
-  return undefined;
+  let n = 1;
+  let goldenRatio = (1 + Math.sqrt(5)) / 2; // φ
+  let conjugate = (1 - Math.sqrt(5)) / 2; // ψ
+  while (true) {
+    let next = Math.round(
+      (Math.pow(goldenRatio, n) - Math.pow(conjugate, n)) / Math.sqrt(5)
+    );
+    n++;
+    yield next;
+  }
 }
